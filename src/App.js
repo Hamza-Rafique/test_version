4bf6746ea92a7efea4f6a1bd14e6ed8ce56b9f8a
Hamza Rafique <hamzarafique964@gmail.com>
2025-04-26 09:44:11 +0000

    updated

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import {
   Routes,
   Route
 } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { WagmiProvider } from 'wagmi'
+import "@rainbow-me/rainbowkit/styles.css";
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+
 import Layout from './layout/Layout';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
@@ -11,24 +16,32 @@ import FAQ from './components/pages/FAQ';
 import NotFound from './components/pages/NotFound';
 import MarketPlace from './components/pages/MarketPlace';
 import SingleProperty from './components/pages/SingleProperty';
-
 import './App.css';
+import { config } from './config';
 
 function App() {
+  const queryClient = new QueryClient()
   return (
     <div className="body-wrap">
-      <Router>
-        <Layout>
-          <Routes>
-            <Route path='/' element={<Home />}></Route>
-            <Route path='/About' element={<About />}></Route>
-            <Route path='/FAQ' element={<FAQ />}></Route>
-            <Route path='/MarketPlace' element={<MarketPlace />}></Route>
-            <Route path='/property/:id' element={<SingleProperty />}></Route>
-            <Route path='*' element={<NotFound />} />
-          </Routes>
-        </Layout>
-      </Router>
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <Router>
+            <Layout>
+              <Routes>
+                <Route path='/' element={<Home />}></Route>
+                <Route path='/About' element={<About />}></Route>
+                <Route path='/FAQ' element={<FAQ />}></Route>
+                <Route path='/MarketPlace' element={<MarketPlace />}></Route>
+                <Route path='/property/:id' element={<SingleProperty />}></Route>
+                <Route path='*' element={<NotFound />} />
+              </Routes>
+            
+            </Layout>
+          </Router>
+          </RainbowKitProvider>
+        </QueryClientProvider>
+      </WagmiProvider>
     </div>
   );
 }
--- src/components/navigations/MenuBar.jsx
+++ src/components/navigations/MenuBar.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-
-import './MenuBar.css'
-
+import './MenuBar.css';
+import {
+    ConnectButton,
+} from '@rainbow-me/rainbowkit';
 
 const MenuBar = () => {
     return (
@@ -9,23 +10,29 @@ const MenuBar = () => {
             <div className="nav-wrapper">
                 <a className="logo" href='/'>
                     <svg width="30" height="35" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <circle cx="15" cy="20" r="10" stroke="#006EFF"/>
-                        <circle cx="15" cy="20" r="6" stroke="#006EFF" strokeWidth="3"/>
-                    </svg>                 
+                        <circle cx="15" cy="20" r="10" stroke="#006EFF" />
+                        <circle cx="15" cy="20" r="6" stroke="#006EFF" strokeWidth="3" />
+                    </svg>
                     GoldenCity
-                    </a>
-                <input className="menu-btn" type="checkbox" id="menu-btn"/>
+                </a>
+                <input className="menu-btn" type="checkbox" id="menu-btn" />
                 <label className="menu-icon" htmlFor="menu-btn"><span className="navicon"></span></label>
 
                 <ul className="menu">
                     <li><a href="/About">About</a></li>
                     <li><a href="/MarketPlace">MarketPlace</a></li>
                     <li><a href="/FAQ">FAQ</a></li>
-                    <li><button className='button'>Connect</button></li>                
+                    <li>
+                        <ConnectButton
+                            showBalance={false}
+                            accountStatus="address"
+                            chainStatus="icon"
+                        />
+                    </li>
                 </ul>
             </div>
         </nav>
-    )
+    );
 }
 
 export default MenuBar;
